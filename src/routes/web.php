1b026ac7ea770d0debf54ef6601cbec752306d2f
Milen Dimitrov <moosenl@webvanced.nl>
2020-01-16 17:26:57 +0200

    middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,8 @@
 Route::group(['namespace'=>'Milen\Contact\Http\Controllers'], function(){
     Route::get('contact', 'ContactController@index')->name('contact');
     Route::post('contact', 'ContactController@send');
-    Route::group(['prefix'=>'api','midleware'=>'api'],function (){
+
+    Route::group(['prefix'=>'api','middleware'=>'api'],function (){
         Route::get('test', function (){
             return json_encode(['key'=>'value']);
         });
