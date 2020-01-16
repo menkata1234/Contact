5e0f95a95d03810edf3cd46d889988e6331b1bed
Milen Dimitrov <moosenl@webvanced.nl>
2020-01-16 17:20:56 +0200

    added route api

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,10 @@
 Route::group(['namespace'=>'Milen\Contact\Http\Controllers'], function(){
     Route::get('contact', 'ContactController@index')->name('contact');
     Route::post('contact', 'ContactController@send');
+    Route::group(['prefix'=>'api','midleware'=>'api'],function (){
+        Route::get('test', function (){
+            return json_encode(['key'=>'value']);
+        });
+    });
 });
 
